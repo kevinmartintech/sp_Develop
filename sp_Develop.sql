7ca34928911760a06a6145feb22953d947abc9c1
Kevin Martin <45496490+kevinmartintech@users.noreply.github.com>
2023-06-10 12:09:10 -0500

    Update sp_Develop.sql

--- sp_Develop.sql
+++ sp_Develop.sql
@@ -28,7 +28,7 @@ AS
         /**********************************************************************************************************************
         ** MIT License
         ** 
-        ** Copyright 2023 Kevin Martin.
+        ** Copyright 2023 Kevin Martin
         ** 
         ** Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
         ** documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
