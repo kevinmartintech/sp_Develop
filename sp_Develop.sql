d88ce6798f2f1ef1f4f49e95ce3f2ce01d2e956c
Kevin Martin <kevin@kevinmartin.tech>
2024-12-16 16:00:49 -0600

    updated version date.

--- sp_Develop.sql
+++ sp_Develop.sql
@@ -28,7 +28,7 @@ AS
         /**********************************************************************************************************************
         ** MIT License
         ** 
-        ** Copyright 2023 Kevin Martin
+        ** Copyright 2024 Kevin Martin
         ** 
         ** Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
         ** documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
@@ -73,8 +73,8 @@ AS
 	    ** Setting some varibles
 	    **********************************************************************************************************************/
 
-        SET @Version = '3.0.3';
-        SET @VersionDate = '20231029';
+        SET @Version = '3.0.4';
+        SET @VersionDate = '20241216';
         SET @URLBase = 'https://www.spdevelop.org/best-practices-and-findings/';
         SET @URLSkipChecks = 'https://www.spdevelop.org/how-to-skip-checks';
         SET @OutputType = UPPER(@OutputType);
